364e6668792d739abc8b0dfa53e489a5ec52e830
Aaron Stannard <aaron@petabridge.com>
2018-12-07 05:06:30 -0800

    checked in NuGet configs

--- PaymentsApp.Client/Program.cs
+++ PaymentsApp.Client/Program.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Akka.Actor;
 
 namespace PaymentsApp.Client
 {
     class Program
     {
+        private static ActorSystem System;
         static void Main(string[] args)
         {
+            System = ActorSystem.Create("paymentsApp");
         }
     }
 }
